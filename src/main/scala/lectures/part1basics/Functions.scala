be6ff7024f31422f8d106b1b3ee95d2850104857
Ian King <ian.king@clearscore.com>
2023-05-23 14:49:19 +0100

    up-to-date

--- src/main/scala/lectures/part1basics/Functions.scala
+++ src/main/scala/lectures/part1basics/Functions.scala
@@ -1,5 +1,7 @@
 package lectures.part1basics
 
+import scala.annotation.tailrec
+
 object Functions extends App {
 
   def aFunction(a: String, b: Int): String = {
@@ -11,25 +13,34 @@ object Functions extends App {
   def aParameterlessFunction(): Int = 42
   println(aParameterlessFunction())
   println(aParameterlessFunction)
+  // Can call a parameterless function with or without the param parenthesis
 
   def aRepeatedFunction(aString: String, n: Int): String = {
     if (n == 0) aString
     else aString + " " + aRepeatedFunction(aString, n - 1)
   }
-
   println(aRepeatedFunction("yup", 5))
 
-  // WHEN YOU NEED LOOPS, USE RECURSION.
+  @tailrec
+  def aRecFunc2(stringInput: String, nTimes: Int, result: String = ""): String = {
+    if (nTimes == 0) result
+    else aRecFunc2(stringInput, nTimes - 1, stringInput + " " + result)
+  }
+  println(aRecFunc2("test", 5))
+
+  // WHEN YOU NEED LOOPS USE RECURSION
+  // Compiler can't figure out return types for recursive functions.
 
 
   def aFunctionWithSideEffects(aString: String): Unit = println(aString)
+  aFunctionWithSideEffects("hellooo")
 
+
+  // can define functions within a code block
   def aBigFunction(n: Int): Int = {
     def aSmallerFunction(a: Int, b: Int) : Int = a + b
-
     aSmallerFunction(n, n - 1)
   }
   println(aBigFunction(5))
-
 
 }
